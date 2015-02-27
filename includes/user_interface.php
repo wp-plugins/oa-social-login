2e8a255c0a5648f8f9bc488ef6ec87d2266d4aa4
ClaudeSchlesser <ClaudeSchlesser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 08:49:01 +0000

    [fix] Missing semicolon for JS
    git-svn-id: https://plugins.svn.wordpress.org/oa-social-login/tags/4.6@1101031 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/user_interface.php
+++ includes/user_interface.php
@@ -35,7 +35,7 @@ function oa_social_login_add_javascripts ()
 			$output [] = '<script type="text/javascript">';
 			$output [] = " (function() {";
 			$output [] = "  var oa = document.createElement('script'); oa.type = 'text/javascript';";
-			$output [] = "  oa.async = true; oa.src = '" . $oneall_js_library . "'";
+			$output [] = "  oa.async = true; oa.src = '" . $oneall_js_library . "';";
 			$output [] = "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oa, s);";
 			$output [] = " })();";
 			$output [] = "</script>";
@@ -623,6 +623,7 @@ function oa_social_login_custom_avatar ($avatar, $mixed, $size, $default, $alt =
 				{
 					$user_picture = (!empty ($user_meta_thumbnail) ? $user_meta_thumbnail : $user_meta_picture);
 				}
+
 
 				//Avatar found?
 				if ($user_picture !== false AND strlen (trim ($user_picture)) > 0)
