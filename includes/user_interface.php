c0e247921e8eecc35a6f70dba9cdcca77b9e78eb
ClaudeSchlesser <ClaudeSchlesser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 09:21:48 +0000

    WP nonce for Social Link callback (RM67)
    git-svn-id: https://plugins.svn.wordpress.org/oa-social-login/trunk@1220747 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/user_interface.php
+++ includes/user_interface.php
@@ -306,10 +306,10 @@ function oa_social_login_render_link_form ($source, $user)
 						//Callback URI
 						$callback_uri = oa_social_login_get_current_url ();
 						$callback_uri .= (strlen (parse_url ($callback_uri, PHP_URL_QUERY)) == 0 ? '?' : '&') . 'oa_social_login_source=' . $source . '#oa_social_link';
-
+						$callback_uri = wp_nonce_url ($callback_uri);
+						
 						//Setup Social Container
 						$containerid = 'oneall_social_login_providers_' . mt_rand (99999, 9999999);
-
 
 						//Setup Social Link
 						$social_link = array ();
